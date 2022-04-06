8d026fb7ead0109f23534070fa367fa09d7c142b
sachinmadhwa <kjhasachin@gmail.com>
2022-04-06 16:59:37 +0545

    lorem generate project done

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,45 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
+  const [count, setCount] = useState(0);
+  const [texts, setText] = useState([]);
+
+  const textSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
+  };
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <>
+      <section className="section-center">
+        <h3>TIRED OF BORING LOREM IPSUM?</h3>
+        <form className="lorem-form">
+          <label>Paragraphs:</label>
+          <input
+            type="number"
+            name="amount"
+            id="amoutn"
+            value={count}
+            onChange={(e) => setCount(e.target.value)}
+          />
+          <button type="submit" onClick={textSubmit} className="btn">
+            generate
+          </button>
+        </form>
+        <article className="lorem-text">
+          {texts.map((line, index) => {
+            return <p key={index}> {line} </p>;
+          })}
+        </article>
+      </section>
+    </>
+  );
 }
 
 export default App;
